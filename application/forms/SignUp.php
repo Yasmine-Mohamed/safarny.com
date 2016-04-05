fcb143142d6278118fed4a1b4b56713bfe74d23e
Yasmine <yasminemohamed2093@gmail.com>
2016-04-05 19:13:35 +0200

    Signup form

---
+++ application/forms/SignUp.php
@@ -0,0 +1,64 @@
+<?php
+
+class Application_Form_SignUp extends Zend_Form
+{
+
+    public function init()
+    {
+        /* Form Elements & Other Definitions Here ... */
+
+        $this->setMethod('POST');
+        $this->setAttrib('class','form-horizontal');
+
+        //Username
+        $username = new Zend_Form_Element_Text('username');
+        $username->setLabel('Username: ');
+        $username->setAttribs(array(
+            'placeholder' => 'Username',
+            'class' => 'form-control',
+        ));
+        $username->setRequired();
+        $username->addFilter('StringTrim');
+
+        //Email
+        $email = new Zend_Form_Element_Text('email');
+        $email->setLabel('Email: ');
+        $email->setAttribs(array(
+            'class' => 'form-control',
+            'placeholder' => 'Email Address'
+        ));
+        $email->setRequired();
+
+        //Password
+        $password = new Zend_Form_Element_Password('password');
+        $password->setLabel('Password: ');
+        $password->setAttribs(array(
+            'class' => 'form-control',
+            'placeholder' => 'Password'
+        ));
+        $password->setRequired();
+
+        //Confirm Password
+        $c_password = new Zend_Form_Element_Password('c_password');
+        $c_password->setLabel('Confirm Password: ');
+        $c_password->setAttribs(array(
+            'class' => 'form-control',
+            'placeholder' => 'Password Again'
+        ));
+        $c_password->setRequired();
+
+        //Submit Button
+        $submit = new Zend_Form_Element_Submit('SignUp');
+        $submit->setAttrib('class','btn btn-primary btn-block btn-lg');
+
+        //add elements
+        $this->addElements(array(
+            $username,
+            $email,
+            $password,
+            $c_password,
+            $submit
+        ));
+    }
+}
+
