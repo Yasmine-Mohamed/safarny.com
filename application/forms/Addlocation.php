e0ea4f989c32abeb4d2962d86c78f1d0f9b2439d
MarwaFawzi <dr.marwafawzi@hotmail.com>
2016-04-09 09:43:27 +0200

    location crud operation for admin

---
+++ application/forms/Addlocation.php
@@ -0,0 +1,65 @@
+<?php
+
+class Application_Form_Addlocation extends Zend_Form
+{
+
+    public function init()
+    {
+        /* Form Elements & Other Definitions Here ... */
+        $this->setMethod('POST');
+
+        $location_name = new Zend_Form_Element_Text('location_name');
+        $location_name->setLabel('Location Name: ');
+        $location_name->setAttribs(Array(
+            'placeholder'=>'Example: Sedi Gaber',
+            'class'=>'form-control'
+        ));
+        $location_name->setRequired();
+
+        $location_name->addFilter('StringTrim');
+
+
+        $location_image = new Zend_Form_Element_Text('location_image');
+        $location_image->setLabel('Location Image: ');
+        $location_image->setAttribs(Array(
+            'placeholder'=>'Example: https://cdn.photographylife.com/wp-content/uploads/2014/06/Nikon-D810-Image-Sample-6.jpg',
+            'class'=>'form-control'
+        ));
+        $location_image->setRequired();
+
+        $location_image->addFilter('StringTrim');
+
+        $city_obj = new Application_Model_City();
+        $allCities = $city_obj->listAll();
+        $city_id = new Zend_Form_Element_Select('city_id');
+
+
+        foreach($allCities as $key=>$value)
+        {
+            $city_id->addMultiOption($value['city_id'], $value['city_name']);
+        }
+        $city_id->setAttribs(Array(
+            'class'=>'form-control'));
+
+
+        $submit=new Zend_Form_Element_Submit('GetYourQuote');
+        $submit->setAttribs(Array(
+            'class'=>'btn btn-block btn-lg btn-success'));
+
+
+        $this->addElements(array(
+            $location_name,
+            $location_image,
+            $city_id,
+            $submit
+        ));
+
+
+    }
+
+
+
+
+
+}
+
--- application/models/City.php
+++ application/models/City.php
@@ -3,7 +3,7 @@
 class Application_Model_City extends Zend_Db_Table_Abstract
 {
     protected $_name = 'city';
-    protected $_dependentTables= array('Application_Model_Car','Application_Model_Hotel');
+    protected $_dependentTables= array('Application_Model_Car','Application_Model_Hotel','Application_Model_Location');
     protected $_referenceMap = array('cities'=>array(
         'columns'=>array('city_id'),
         'refTableClass'=>'Application_Model_Country',
@@ -18,6 +18,11 @@ class Application_Model_City extends Zend_Db_Table_Abstract
 
     public function find_city_country($country_id){
         return $this->fetchAll("country_id=$country_id")->toArray();
+    }
+
+    public function listAll()
+    {
+        return $this->fetchAll()->toArray();
     }
 }
 
